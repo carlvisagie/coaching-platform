d75fd34d70d0ba2ed18190b315891971dd422fd4
Carl Visagie <carlvisagie@gmail.com>
2025-05-17 17:31:07 +0200

    Moved static build to backend and updated main.py

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,10 @@
-
 import React from 'react';
-import { HashRouter, Routes, Route } from "react-router-dom";
-import EmotionDashboard from "./EmotionDashboard";
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<h1>Welcome to the Coaching Platform</h1>} />
-        <Route path="/dashboard" element={<EmotionDashboard />} />
-      </Routes>
-    </HashRouter>
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to the Coaching Platform</h1>
+    </div>
   );
 }
 
